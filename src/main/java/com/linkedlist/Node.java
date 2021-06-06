54522ae5c1e6dd6c76c9bd0f46fa641b1782fc42
Naveen <naveentruly86@gmail.com>
2021-06-06 23:46:43 +0530

    Added: Create a Linked List

--- src/main/java/com/linkedlist/Node.java
+++ src/main/java/com/linkedlist/Node.java
@@ -1,4 +1,19 @@
 package com.linkedlist;
 
-public class Node {
+public class Node<K> {
+    private Node next;
+    private K key;
+
+    public Node(K key) {
+        this.key = null;
+        this.next = null;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
