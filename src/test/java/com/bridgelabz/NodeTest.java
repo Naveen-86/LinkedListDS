a0619cb9f56f08f1710815bc7ba4fbbd2a286ab8
Naveen <naveentruly86@gmail.com>
2021-06-21 09:59:52 +0530

    Modified the LinkedList by Adding Two Nodes to One Node

---
+++ src/test/java/com/bridgelabz/NodeTest.java
@@ -0,0 +1,34 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class NodeTest {
+    @Test
+    public void Given3Numbers_WhenLinked_ShouldPassed_LinkedListTest() {
+        Node<Integer> firstNode = new Node<Integer>(56);
+        Node<Integer> secondNode = new Node<Integer>(30);
+        Node<Integer> thirdNode = new Node<Integer>(70);
+        firstNode.setNext(secondNode);
+        secondNode.setNext(thirdNode);
+        boolean res = firstNode.getNext().equals(secondNode) &&
+                secondNode.getNext().equals(thirdNode);
+        Assertions.assertTrue(res);
+    }
+
+    @Test
+    public void Given3Numbers_WhenAdded_ToLinkedListShould_BeAddedOnTop() {
+        Node<Integer> firstNode = new Node<Integer>(70);
+        Node<Integer> secondNode = new Node<Integer>(30);
+        Node<Integer> thirdNode = new Node<Integer>(56);
+        NewLinkList linkedList = new NewLinkList();
+        linkedList.add(firstNode);
+        linkedList.add(secondNode);
+        linkedList.add(thirdNode);
+        linkedList.printNodes();
+        boolean res = linkedList.head.equals(thirdNode) &&
+                linkedList.head.getNext().equals(secondNode) &&
+                linkedList.tail.equals(firstNode);
+        Assertions.assertTrue(res);
+    }
+}
